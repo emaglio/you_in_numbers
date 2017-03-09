f9a894b2d99ba20f42547ab89a9ff775740508ed
emasurf85 <emanuele.magliozzi@gmail.com>
2017-03-09 15:34:18 +1100

    cpet data imported - wrong format for time and params #3

--- test/test.rb
+++ test/test.rb
@@ -17,6 +17,10 @@ module MyReport
 
   class Op
     
+    def is_number?(string)
+      true if Float(string) rescue false
+    end
+
     # find the bigger sheet I suppose the data are saved (maybe use always the first sheet)
     # and set it as defualt one
     def set_default_sheet(file)
@@ -35,15 +39,43 @@ module MyReport
 
     # find list of column index for all the parameters
     def cpet_params_col(file)
-      params = {"t" => 0, "Rf" => 0, "VO2" => 0, "VCO2" => 0}
+      params = {"t" => [], "time" => [], "Rf" => [], "VE" => [], "VO2" => [], "VCO2" => [], 
+                "RQ" => [], "VE/VO2" => [], "HR" => [], "VO2/Kg" => [], "FAT%" => [], "CHO%" => [],
+                "Power" => [], "Revolution" => [], "Phase" => []
+                }
 
       params_header = file.row(1)
+      params_col = []
 
+      # TODO: needs to find the params with different format ex: VO2, Vo2, vo2
       params.each do |key, value|
-        params[key] = params_header.index(key)
+        params_col << params_header.index(key)
       end
 
+      # using the VO2 columns
+      # TODO: check on params_col, heandle the situtation if params_index is nil
+      first_num = 0
+      temp_array = []
+      temp_array = file.column(params_col[4])
+
+      # find first number of the columns
+      temp_array.each do |value|
+        is_number?(value) ? break : first_num += 1
+      end
+
+      row = first_num + 1
+      file.each_row_streaming do
+        i = 0
+        params.each do |key, value|
+          params[key] << file.cell(row, params_col[i]) if params_col[i] != nil
+          i += 1
+        end
+        row += 1
+      end
+
+
       puts params.inspect
+      puts params_col.inspect
 
 
     end
