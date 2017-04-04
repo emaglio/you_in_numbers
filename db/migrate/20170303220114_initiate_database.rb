cd3ba92b3b7d69f821a46af26469e0d860e5d2b2
emaglio <emanuele.magliozzi@gmail.com>
2017-04-05 09:56:11 +1000

    Added Company

--- db/migrate/20170303220114_initiate_database.rb
+++ db/migrate/20170303220114_initiate_database.rb
@@ -15,6 +15,7 @@ class InitiateDatabase < ActiveRecord::Migration
     end
 
     create_table :reports do |t|
+      t.integer :user_id
       t.string :title
       t.text :cpet_params
       t.text :cpet_results
@@ -23,6 +24,23 @@ class InitiateDatabase < ActiveRecord::Migration
       t.text :header
       t.text :subject
       t.text :content
+
+      t.timestamps
+    end
+
+    create_table :conmpany do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :addres_1
+      t.string :addres_2
+      t.string :city
+      t.string :poscode
+      t.string :country
+      t.string :phone
+      t.string :email
+      t.text :content
+
+      t.text :logo_meta_data
 
       t.timestamps
     end
