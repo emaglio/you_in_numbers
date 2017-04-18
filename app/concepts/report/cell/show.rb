d4a256190a5a2849e87e93dd4917461daabb0839
emaglio <emanuele.magliozzi@gmail.com>
2017-04-18 18:30:39 +1000

    All main features for charts are implemented (2x dataset, AT and exercise phase) #4 - TODO: edit AT

--- app/concepts/report/cell/show.rb
+++ app/concepts/report/cell/show.rb
@@ -22,17 +22,35 @@ module Report::Cell
       model["cpet_params"]["t"].inspect
     end
 
-    def at
-      at_index = model["cpet_results"]["at_index"] + model["cpet_results"]["exer_phase"]["starts"]
-      return at_index
+    def at_index
+      model["cpet_results"]["at_index"] + model["cpet_results"]["exer_phase"]["starts"]
+    end
+
+    def at_value
+      model["cpet_params"]["t"][at_index].inspect
+    end
+
+    def exer_phase_starts
+      model["cpet_results"]["exer_phase"]["starts"]
+    end
+
+    def exer_phase_ends
+      model["cpet_results"]["exer_phase"]["starts"] + model["cpet_results"]["exer_phase"]["num_steps"]
     end
 
     def exer_phase
       exer_phase_array = []
-      exer_phase_array[0] = model["cpet_results"]["exer_phase"]["starts"]
-      exer_phase_array[1] = model["cpet_results"]["exer_phase"]["starts"] + model["cpet_results"]["exer_phase"]["num_steps"]
+      exer_phase_array[0] = model["cpet_params"]["t"][exer_phase_starts].inspect
+      exer_phase_array[1] = model["cpet_params"]["t"][exer_phase_ends].inspect
 
       return exer_phase_array
+    end
+
+    def y_exer_phase #for exersice phase
+      array = []
+      array[0] = model["cpet_params"]["VO2"].max
+      array[1] = model["cpet_params"]["VCO2"].max
+      return (array.max + 200).round(-2)
     end
   end
 
