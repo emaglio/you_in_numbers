3c376481d58933fe4a07123a16aa01b9681b9903
emaglio <emanuele.magliozzi@gmail.com>
2017-04-19 18:26:13 +1000

    Started with integration test for Company #4

--- app/concepts/user/operation/change_password.rb
+++ app/concepts/user/operation/change_password.rb
@@ -1,15 +1,15 @@
-require 'reform/form/dry' 
+require 'reform/form/dry'
 
 class User::ChangePassword < Tyrant::ChangePassword
   failure :raise_error!
-  step :notify!
+  # step :notify!
 
   def raise_error!(options, *)
     raise ApplicationController::NotAuthorizedError if options["result.policy.default"].failure?
   end
 
-  def notify!(options, current_user:, **)
-    Notification::User.({}, "email" => current_user.email, "type" => "change_password")
-  end
+  # def notify!(options, current_user:, **)
+  #   Notification::User.({}, "email" => current_user.email, "type" => "change_password")
+  # end
 
 end
