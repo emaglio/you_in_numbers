7d546b2f48782f79d80e061e8f18f2b396b4e1ee
emaglio <emanuele.magliozzi@gmail.com>
2017-04-24 11:25:02 +1000

    Implemented summary table for VO2max test #4

--- app/concepts/user/contract/report_settings.rb
+++ app/concepts/user/contract/report_settings.rb
@@ -10,8 +10,19 @@ module User::Contract
     property :content, field: :hash do
       property :params_list
       property :report_path
+      property :training_zones_levels
     end
 
+    property :fat_burning_1, virtual: true
+    property :fat_burning_2, virtual: true
+    property :endurance_1, virtual: true
+    property :endurance_2, virtual: true
+    property :at_1, virtual: true
+    property :at_2, virtual: true
+    property :vo2max_1, virtual: true
+    property :vo2max_2, virtual: true
+
+    unnest :training_zones_levels, from: :content
     unnest :params_list, from: :content
     unnest :report_path, from: :content
   end
