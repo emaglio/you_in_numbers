dfc67e382575d872881382ebbd15ca946c6e1a11
emaglio <emanuele.magliozzi@gmail.com>
2017-05-18 18:24:04 +1000

    Added error handler #10

--- app/concepts/report/operation/generate_image.rb
+++ app/concepts/report/operation/generate_image.rb
@@ -3,10 +3,16 @@ class Report::GenerateImage < Trailblazer::Operation
   step Policy::Pundit( ::Session::Policy, :report_company_owner? )
   failure ::Session::Lib::ThrowException
   step :generate_image!
+  failure :error!
 
   def generate_image!(options, params:, **)
+    return false if params[:error]
     file = File.open("#{Rails.root}/public/temp_files/image-#{params[:index]}.png", "wb")
     file.write(params[:image].read)
+  end
+
+  def error!(options, *)
+    options["error"] = "Something went wrong"
   end
 
 end # class Report::GenerateImage
