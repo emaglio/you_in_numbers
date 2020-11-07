c0700be7a15f4bfa7926c0f9f28b3b85b6294267
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2020-11-07 18:35:49 +1100

    more userfriendly puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,37 @@
 require 'puma'
 
-workers_count = ENV.fetch('PUMA_WORKERS', 1).to_i
 threads_count = ENV.fetch('PUMA_THREADS', 16).to_i
-
-workers workers_count
 threads 1, threads_count
 
-environment ENV.fetch('RAILS_ENV', 'production')
+rails_env = ENV.fetch("RAILS_ENV") { "development" }
+environment rails_env
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+directory app_dir
 
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+if %w[production staging].member?(rails_env)
+  workers_count = ENV.fetch('PUMA_WORKERS', 1).to_i
+  workers workers_count
 
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+  shared_dir = "#{app_dir}/shared"
 
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
+  # Set up socket location
+  bind "unix://#{shared_dir}/sockets/puma.sock"
 
-on_worker_boot do
- require "active_record"
- ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
- ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  # Logging
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+  # Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+  activate_control_app
+
+  on_worker_boot do
+   require "active_record"
+   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
+elsif rails_env == "development"
+  port   ENV.fetch("PORT") { 3000 }
+  plugin :tmp_restart
 end
