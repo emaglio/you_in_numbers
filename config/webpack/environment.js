e2b2b5bda8ef8d854f61ae1c3498537be1c1ca8f
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2021-01-02 10:09:04 +1100

    Using bootstrap 3 instead of 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const customConfig = {
-  resolve: {
-    extensions: ['.css']
-  }
-}
 
 const styleLoader = {
   test: /\.scss$/,
@@ -18,6 +13,31 @@ const styleLoader = {
   ]
 }
 
-// environment.config.merge(customConfig)
-// environment.loaders.append('style', styleLoader)
+const woffFontLoader = {
+  test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+  use: [
+    { loader: 'url-loader?limit=10000&mimetype=application/font-woff' }
+  ]
+}
+
+const ttfFontLoader = {
+  test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+  use: [
+    { loader: 'url-loader?limit=10000&mimetype=application/octet-stream' }
+  ]
+}
+
+const eotFontLoader = {
+  test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+  use: [
+    { loader: 'file-loader' }
+  ]
+}
+
+environment.loaders.append('woffFont', woffFontLoader)
+environment.loaders.append('ttfFont', ttfFontLoader)
+environment.loaders.append('eotFont', eotFontLoader)
+environment.loaders.prepend('style', styleLoader)
+
+console.log(environment.loaders)
 module.exports = environment
