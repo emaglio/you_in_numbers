c6f4496a64f1cfa2843ac3bb9d6cb82906b2fcc2
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2021-02-16 21:19:56 +1100

    Fix seeds after TRB upgrade

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,41 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User::Operation::Create.({
-                      firstname: 'Admin',
-                      lastname: 'Imp',
-                      email: 'admin@email.com',
-                      password: 'Test1234',
-                      confirm_password: 'Test1234' })['model']
+admin = User::Operation::Create.(
+  params: {
+    firstname: 'Admin',
+    lastname: 'Imp',
+    email: 'admin@email.com',
+    password: 'Test1234',
+    confirm_password: 'Test1234',
+    admin: true
+  })[:model]
 puts 'Admin account created successfully!' if admin.id
 
-subject = Subject::Operation::Create.({
-                          user_id: admin.id,
-                          firstname: 'Ema',
-                          lastname: 'Maglio',
-                          gender: 'Male',
-                          dob: '01/01/1980',
-                          height: '180',
-                          weight: '80',
-                          phone: '912873',
-                          email: 's@e.com'
-                          }, 'current_user' => admin)['model']
+subject = Subject::Operation::Create.(
+  params: {
+    user_id: admin.id,
+    firstname: 'Ema',
+    lastname: 'Maglio',
+    gender: 'Male',
+    dob: '01/01/1980',
+    height: '180',
+    weight: '80',
+    phone: '912873',
+    email: 's@e.com'
+  }, current_user: admin)[:model]
 puts 'Dummy Subject created successfully!' if subject.id
 
 upload_file = ActionDispatch::Http::UploadedFile.new({
   :tempfile => File.new(Rails.root.join('test/files/cpet.xlsx'))
 })
 
-report = Report::Operation::Create.({
-          user_id: admin.id,
-          subject_id: subject.id,
-          title: 'My report',
-          cpet_file_path: upload_file,
-          template: 'default'
-      }, 'current_user' => admin)['model']
+report = Report::Operation::Create.(
+  params: {
+      user_id: admin.id,
+      subject_id: subject.id,
+      title: 'My report',
+      cpet_file_path: upload_file,
+      template: 'default'
+  }, current_user => admin)[:model]
 puts 'Dummy Report created successfully!' if report.id
